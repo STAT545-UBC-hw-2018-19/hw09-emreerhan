38736d60224c2194d6b19715837b167b92baa9d3
eerhan <eerhan@sfu.ca>
2018-11-27 12:52:42 -0800

    Remove frequency calculation, since ggplot histogram does it anyway

--- ngram-histogram.py
+++ ngram-histogram.py
@@ -6,14 +6,12 @@ def main():
     words_file = sys.argv[1]
     # Second param is size of n-grams
     n = sys.argv[2]
-    ngrams_freq = dict()
     with open(words_file, 'r') as words:
         word = words.readline().strip().lower()
         while not word is '':
             for ngram in get_ngrams(word, int(n)):
-                ngrams_freq[ngram] = ngrams_freq.get(ngram, 0) + 1 # Add 1 to frequency
+                print(ngram)
             word = words.readline().strip().lower()
-    print(ngrams_freq)
 
 
 def get_ngrams(word, n = 3):
